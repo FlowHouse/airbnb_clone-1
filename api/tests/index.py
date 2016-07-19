3bb9bcf873f571d82b4cb9b254aaba555242b012
butcallmejo <josquin.gaillard@gmail.com>
2016-07-18 17:04:08 -0700

    got that first testing part donegit add .git add .

--- api/tests/index.py
+++ api/tests/index.py
@@ -1,9 +1,9 @@
 # test github pull from head to forked repo
 import unittest
-import datetime
+from datetime import datetime
 import json
-import requests
-import app
+from flask_json import FlaskJSON, jsonify, JsonTestResponse
+from app import *
 
 class BaseTest(unittest.TestCase):
 	"""docstring for BaseTest"""
@@ -12,27 +12,35 @@ class BaseTest(unittest.TestCase):
 	# to create a test client of app
 	def setUp(self):
 		self.app = app.test_client()
+        app.response_class = JsonTestResponse
 
 	# to validate if status of the JSON resp of GET / is equal to 200
 	def test_200(self):
+		response = self.app.get('/')
 		assert response.status_code == 200
 
 	# to validate if status of the JSON resp of GET / is equal to OK
 	def test_status(self):
-		data = json.loads(response.text)
-		assert data['status'] == 'OK'
+		response = self.app.get('/')
+		# print response
+		# json_r = json.dumps(response)
+		# data = json.loads(response)
+		# print data
+		assert response.json['status'] == 'OK'
 
 	# to validate if time of the JSON resp of GET / is equal of local time
 	def test_time(self):
-		dt = str(datetime.datetime.now())
-		data = json.loads(response.text)
-		assert data['time'] == dt
+		response = self.app.get('/')
+		dt = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+		# data = json.loads(response.data)
+		assert response.json['time'] == dt
 
 	# to validate if utc time of the JSON resp of GET / is equal of utc time
 	def test_time_utc(self):
-		dt = str(datetime.datetime.utcnow())
-		data = json.loads(response.text)
-		assert data['utc_time'] == dt
+		response = self.app.get('/')
+		dt = datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S")
+		# data = json.loads(response.data)
+		assert response.json['utc_time'] == dt
 
 if __name__ == '__main__':
 	unittest.main()
