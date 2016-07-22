810a448d631aa7ad2049667979518f2b11f51c7b
Kris Bredemeier <34@holbertonschool.com>
2016-07-22 11:02:36 -0700

    works on test_list function

--- api/tests/user.py
+++ api/tests/user.py
@@ -8,26 +8,67 @@ class UserTest(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
-        app.response_class = JsonTestResponse
-        database.connect()
-        # database.creat_tables([User], safe=True)
-        cursor = database.curser()
-        cursor.execute("CREATE TABLE User")
         logging.disable(logging.CRITICAL)
+        db.connect()
+        db.creat_tables([User], safe=True)
+        # cursor = database.curser()
+        # cursor.execute("CREATE TABLE User")
 
     def tearDown(self):
-        # database.drop_tables([User], safe=True)
-        cursor().execute("DROP TABLE IF EXISTS User"
-        os.close(self.app)
+        db.drop_tables([User], safe=True)
+        # cursor().execute("DROP TABLE IF EXISTS User"
+        # os.close(self.app)
 
     def test_create(self):
         self.app.post('/users', data=dict (
-            username=username,
-            password=password
-        ), follow_redirects=True)
+            first_name='first_name',
+            last_name='last_name',
+            email='eamil@1',
+            password='password'
+        ))
+        set.assertEqual(User.select(id), 1)
 
-    # def test_list(self):
-    #
+        self.app.post('/users', data=dict (
+            first_name=' ',
+            last_name='last_name',
+            email='email@2',
+            password='password'
+        ))
+        set.assertEqual(User.select(id), 2)
+
+        self.app.post('/users', data=dict (
+            first_name='first_name',
+            last_name=' ',
+            email='email@3',
+            password='password'
+        ))
+        set.assertEqual(User.select(id), 3)
+
+        self.app.post('/users', data=dict (
+            first_name='first_name',
+            last_name='last_name',
+            email=' ',
+            password='password'
+        ))
+        set.assertEqual(User.select(id), 4)
+
+        self.app.post('/users', data=dict (
+            first_name='first_name',
+            last_name='last_name',
+            email='email@5',
+            password=' '
+        ))
+        set.assertEqual(User.select(id), 5)
+
+
+    def test_list(self):
+        list_test = self.app.get('/users')
+        try:
+            to_dict = json.loads(list_test.data)
+            return len(to_dict)
+        except:
+            return 0
+
     # def test_get(self):
     #
     # def test_delete(self):
