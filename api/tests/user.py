0fcf0fb0d70b7141eec7df2a823e5442c64da905
butcallmejo <josquin.gaillard@gmail.com>
2016-07-23 14:34:22 -0700

    refactoring of user.py

--- api/tests/user.py
+++ api/tests/user.py
@@ -3,6 +3,8 @@ import logging
 from datetime import datetime
 import json
 from app.models.base import *
+from app.models.user import *
+from app.views.user import *
 from flask_json import FlaskJSON, jsonify, JsonTestResponse
 from app import *
 
@@ -23,64 +25,51 @@ class UserTest(unittest.TestCase):
         # cursor().execute("DROP TABLE IF EXISTS User"
         # os.close(self.app)
 
-        #validate if the POST request => POST /users
+	# func to create user for testing purposes
+	# def create_user(self, first, last, email, pwd):
+	# 	# uses post to add a user to the User table
+	# 	return self.app.post('/users', data=dict (
+    #         first_name=first,
+    #         last_name=last,
+    #         email=email,
+    #         password=pwd
+    #     ))
+
+    #validate if the POST request => POST /users
     def test_create(self):
-        #create a user if all required parameters are sent
-        self.app.post('/users', data=dict (
-            first_name='first_name',
-            last_name='last_name',
-            email='eamil@1',
-            password='password'
-        ))
-        #check the user ID
-        set.assertEqual(User.select(id), 1)
+		#create a user if all required parameters are sent
+		# TODO TODO TODO find out what goes in bottom TODO TODO TODO
 
-        #test all cases of missing parameters
+		#check the user ID
+		user1 = create_user()
+		self.assertEqual(user1, 1)
 
-        #missing first_name
-        self.app.post('/users', data=dict (
-            first_name=' ',
-            last_name='last_name',
-            email='email@2',
-            password='password'
-        ))
-        set.assertEqual(User.select(id), 2)
+		#test all cases of missing parameters
 
-        #missing last_name
-        self.app.post('/users', data=dict (
-            first_name='first_name',
-            last_name=' ',
-            email='email@3',
-            password='password'
-        ))
-        set.assertEqual(User.select(id), 3)
+		#missing first_name
+		user2 = create_user()
+		self.assertEqual(user2, 2)
 
-        #missing email
-        self.app.post('/users', data=dict (
-            first_name='first_name',
-            last_name='last_name',
-            email=' ',
-            password='password'
-        ))
-        set.assertEqual(User.select(id), 4)
+		#missing last_name
+		user3 = create_user()
+		self.assertEqual(user3, 3)
 
-        #missing password
-        self.app.post('/users', data=dict (
-            first_name='first_name',
-            last_name='last_name',
-            email='email@5',
-            password=' '
-        ))
-        set.assertEqual(User.select(id), 5)
+		#missing email
+		user4 = create_user()
+		self.assertEqual(user4, 4)
 
-        #check if an user has unique email
-        unique_email_test = self.app.post('/users', data=dict(
-            first_name='first_name',
-            last_name='last_name',
-            email='email',
-            password='passwrod'
-            ))
-        assert unique_email_test.status == 409
+		#missing password
+		user5 = create_user()
+		self.assertEqual(user5, 5)
+
+		#check if an user has unique email
+		unique_email_test = self.app.post('/users', data=dict(
+			first_name='first_name',
+			last_name='last_name',
+			email='email',
+			password='passwrod'
+		))
+		assert unique_email_test.status == 409
 
     #validate if the GET request => GET /users:
     def test_list(self):
@@ -99,11 +88,11 @@ class UserTest(unittest.TestCase):
         new_user = self.app.post('/users', data=dict (
             first_name='first_name',
             last_name='last_name',
-            email='email@1',
+            email='email@2',
             password='password'
         ))
         #check the status code
-        assert new_user.status_code == 200
+        assert new_user.status_code == 201
         #this is the getting
         response = self.app.get('/users/1').data
         #check if it's the same resource as during the creation
@@ -127,7 +116,7 @@ class UserTest(unittest.TestCase):
             password='password'
         ))
         #check the status code
-        assert new_user.status_code == 200
+        assert new_user.status_code == 201
         list_test = self.app.get('/users')
         to_dict = json.loadds(list_test.data)
         #check the number of element before and after a delete
@@ -150,7 +139,7 @@ class UserTest(unittest.TestCase):
             email='email@1',
             password='password'
         ))
-        assert new_user.status_code == 200
+        assert new_user.status_code == 201
 
         #this is the updating
         #first name
@@ -160,28 +149,28 @@ class UserTest(unittest.TestCase):
         #check the impact of each request parameters
         list_test = self.app.get('/users/1')
         #check the status code
-        assert new_user.status_code == 200
+        assert new_user.status_code == 201
 
         #last name
         self.app.put('/user/1', data=dict (
             last_name='Holberton'
         ))
         list_test = self.app.get('/users/1')
-        assert new_user.status_code == 200
+        assert new_user.status_code == 201
 
         #email
         self.app.put('/user/1', data=dict (
             email='betty@holberton'
         ))
         list_test = self.app.get('/users/1')
-        assert new_user.status_code == 200
+        assert new_user.status_code == 201
 
         #password
         self.app.put('/user/1', data=dict (
             password='holbertonschool'
         ))
         list_test = self.app.get('/users/1')
-        assert new_user.status_code == 200
+        assert new_user.status_code == 201
 
         #check when trying to update an unknown user
         #try unknown user
